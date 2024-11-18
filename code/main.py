4042b88ec7ffa35c4f1c3a9825db4c6fe1f72a70
pyoseung74 <132826720+pyoseung74@users.noreply.github.com>
2024-11-18 11:54:19 +0900

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,5 @@
+from mod1 import add, name 
+import app.mod2
+result = app.mod2.mul(2000,20)
+print("main.py", __name__)
+print(result, name)
---
+++ code/mod2.py
@@ -0,0 +1,5 @@
+def mul(a, b):
+    return a * b
+    print("import Mod 2 ")
+if __name__ == "__main__":
+    print("mod2 module is executed directly")
